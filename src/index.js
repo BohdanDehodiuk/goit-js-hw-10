21804cdfa0f70863eff88008f99f9eaf37dfc803
BohdanDehodiuk <86683356+BohdanDehodiuk@users.noreply.github.com>
2021-12-07 21:20:23 +0200

    finish hw#10

--- src/index.js
+++ src/index.js
@@ -1,3 +1,41 @@
 import './css/styles.css';
-
+import CountriesApiService from './js/fetchCountries';
+import countriesList from './templates/countries-list';
+import countryCard from './templates/country-card';
+// =====================================================
+const debounce = require('lodash.debounce');
 const DEBOUNCE_DELAY = 300;
+const refs = {
+  inputTag: document.getElementById('search-box'),
+  listTag: document.querySelector('.country-list'),
+  divTag: document.querySelector('.country-info'),
+};
+// ======================================================
+refs.inputTag.addEventListener('input', debounce(onSearchCountries, DEBOUNCE_DELAY));
+const countriesApiService = new CountriesApiService();
+
+function onSearchCountries(e) {
+  refs.listTag.innerHTML = '';
+  refs.divTag.innerHTML = '';
+  countriesApiService.value = e.target.value.trim();
+
+  countriesApiService.fetchCountries().then(countries => {
+    makeCountyCard(countries);
+  });
+}
+// ======================================================
+function makeCountyCard(countries) {
+  return countries.map(({ name, capital, population, flags, languages }) => {
+    console.log({ languages });
+    if (countries.length === 1) {
+      return refs.divTag.insertAdjacentHTML(
+        'afterbegin',
+        countryCard({ name, capital, population, flags, languages }),
+      );
+    }
+    refs.listTag.insertAdjacentHTML(
+      'afterbegin',
+      countriesList({ name, capital, population, flags, languages }),
+    );
+  });
+}
---
+++ src/js/fetchCountries.js
@@ -0,0 +1,29 @@
+import Notiflix from 'notiflix';
+export default class DataFetch {
+  constructor() {
+    this.inputValue = '';
+    console.log(this.inputValue);
+  }
+  fetchCountries() {
+    const URL = `https://restcountries.com/v3.1/name/${this.inputValue}?fields=name,capital,population,flags,languages,`;
+    return fetch(URL)
+      .then(response => response.json())
+      .then(countries => {
+        if (countries.status === 404) {
+          return Notiflix.Notify.failure(`"Oops, there is no country with that name"`);
+        } else if (countries.length > 10) {
+          return Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
+        }
+        return countries;
+      })
+      .catch(e => {
+        Notiflix.Notify.failure(`"Oops, there is no country with that name"`);
+      });
+  }
+  get value() {
+    return this.inputValue;
+  }
+  set value(country) {
+    this.inputValue = country;
+  }
+}
